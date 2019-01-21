15801941285d23a35773709d59ec0671ded4c639
Johan Sorette <sorette.johan@gmail.com>
2019-01-21 16:50:41 +0100

    Add API endpoint calls module

---
+++ src/api/requests.config.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_ROOT = "http://localhost:8080";
+
+const responseData = (res: any) => res.data;
+
+export const get = (url: string) => axios.get(`${API_ROOT}${url}`).then(responseData);
+
+export const post = (url: string, data: any) => axios.post(`${API_ROOT}${url}`, data).then(responseData);
+
+export const put = (url: string, data: any) => axios.put(`${API_ROOT}${url}`, data).then(responseData);
+
+export const del = (url: string) => axios.delete(`${API_ROOT}${url}`).then(responseData);
